91f14d9a6249424bee5fca89869c1c3437919891
David Wolf <d.wolf@live.at>
2014-02-11 09:17:05 +0100

    Lizenz geändert.

--- Framework/Integer.cpp
+++ Framework/Integer.cpp
@@ -1,16 +1,18 @@
-/* This file is part of LupusFramwork.
+/* Copyright © 2014 David Wolf <d.wolf@live.at>
+ *
+ * This file is part of LupusFramwork.
  *
  * LupusFramwork is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * LupusFramwork is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with LupusFramwork.  If not, see <http://www.gnu.org/licenses/>.
  */
 
